ea4a5a7a9163f804b33528cd4808c1bf4bbfb37c
Dave1101 <davidb1101@gmail.com>
2020-09-29 23:16:24 -0500

    Added validation of email address

--- goGame/Controllers/RegistrationController.cs
+++ goGame/Controllers/RegistrationController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace goGame.Controllers
 {
 	public class RegistrationController : Controller
 	{
+		const string emailValidationExpression = @"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$";
+
 		public IActionResult Index()
 		{
 			return View();
@@ -22,7 +25,19 @@ namespace goGame.Controllers
 		public IActionResult Register(string email)
 		{
 			ViewData["registeredEmail"] = email;
+
+
+			if (Regex.IsMatch(email, emailValidationExpression,RegexOptions.IgnoreCase))
+			{
+				ViewData["validEmail"] = true;
+
+			}
+			else
+			{
+				ViewData["validEmail"] = false;
+			}
 			return View();
+
 		}
 	}
 }
