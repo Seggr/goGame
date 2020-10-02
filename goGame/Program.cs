6cd1809a8354c612ee54c1f71021cc05364eb287
Dave1101 <davidb1101@gmail.com>
2020-10-01 19:18:43 -0500

    Taking input from user

--- goGame/Program.cs
+++ goGame/Program.cs
@@ -6,7 +6,19 @@ namespace goGame
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string email;
+
+			Console.WriteLine("Welcome to the game of Go! Masters of Chess!\n\n\n");
+			Console.WriteLine("Enter your email address to register.");
+			Console.Write("Email:");
+			email = Console.ReadLine();
+
+
+			// ^(?(")(".+?(?<!\\)"@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$
+
+
+
+			Console.WriteLine($"Hello World! Your email is {email}");
 		}
 	}
 }
