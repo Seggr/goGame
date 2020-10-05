f000a7a72b1e4e7ced7823476045fe22bcb53819
Dave1101 <davidb1101@gmail.com>
2020-10-04 20:35:12 -0500

    Able to retrieve queues Moved ServiceBus connection string to app.config

--- goGame/Program.cs
+++ goGame/Program.cs
@@ -11,12 +11,10 @@ namespace goGame
 	{
 		static async Task Main(string[] args)
 		{
-			const string ServiceBusConnectionString = "Endpoint=sb://fsx-sb-db.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fzB4b39YYFr5jVDzxkFTH44RuiqbqGfPixWHGjJBNWw=";
-			const string QueueName = "testqueue";
+			string ServiceBusConnectionString = ConfigurationManager.AppSettings.Get("ServiceBusConnectionString");
 			IQueueClient queueClient;
 			var managementClient = new ManagementClient(ServiceBusConnectionString);
 
-			string value = ConfigurationManager.AppSettings.Get("Key0");
 
 
 			const string emailValidationRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
@@ -33,10 +31,19 @@ namespace goGame
 				Console.WriteLine($"Hello World! Your email is {email}");
 
 				email = email.Replace("@", "_");
-				await managementClient.CreateQueueAsync($"{email}");
 
-				Console.WriteLine("Queue created!");
 
+				var currentQueues = await managementClient.GetQueuesAsync();
+
+				if (currentQueues.Contains(new QueueDescription(email)))
+				{
+					Console.WriteLine("Email already registered");
+				}
+				else
+				{
+					await managementClient.CreateQueueAsync($"{email}");
+					Console.WriteLine("Queue created!");
+				}
 
 				//Task taskA = new Task(() => { });
 				//taskA.Start();
@@ -47,7 +54,6 @@ namespace goGame
 				Console.WriteLine($"{email} is not a valid email address.");
 			}
 
-			Console.WriteLine(value);
 
 
 
