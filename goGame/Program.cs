f87f42c41da2b63fb23f0e023499f64ae38174b3
Dave1101 <davidb1101@gmail.com>
2020-10-04 20:22:21 -0500

    Able to create a queue now

--- goGame/Program.cs
+++ goGame/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Configuration;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using System.Threading.Tasks;
 
- 
 namespace goGame
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
+			const string ServiceBusConnectionString = "Endpoint=sb://fsx-sb-db.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fzB4b39YYFr5jVDzxkFTH44RuiqbqGfPixWHGjJBNWw=";
+			const string QueueName = "testqueue";
+			IQueueClient queueClient;
+			var managementClient = new ManagementClient(ServiceBusConnectionString);
+
 			string value = ConfigurationManager.AppSettings.Get("Key0");
 
 
@@ -21,9 +28,19 @@ namespace goGame
 			email = Console.ReadLine();
 
 
-			if(Regex.IsMatch(email, emailValidationRegex, RegexOptions.IgnoreCase))
+			if (Regex.IsMatch(email, emailValidationRegex, RegexOptions.IgnoreCase))
 			{
 				Console.WriteLine($"Hello World! Your email is {email}");
+
+				email = email.Replace("@", "_");
+				await managementClient.CreateQueueAsync($"{email}");
+
+				Console.WriteLine("Queue created!");
+
+
+				//Task taskA = new Task(() => { });
+				//taskA.Start();
+				//taskA.Wait();
 			}
 			else
 			{
@@ -31,6 +48,8 @@ namespace goGame
 			}
 
 			Console.WriteLine(value);
+
+
 
 		}
 	}
