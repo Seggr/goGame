11cb6c819485b45ef16710a81e1cc6d689863c50
David Bates <dbates@fileandserve.com>
2020-10-06 01:16:50 -0500

    Created a start menu with an option to register or login

--- goGame/Program.cs
+++ goGame/Program.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.ServiceBus.Management;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace goGame
 {
@@ -12,20 +13,43 @@ namespace goGame
         const string emailValidationRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
         static async Task Main(string[] args)
         {
-            bool playerRegistered = false;
+            string userInput = "";
 
-            while (!playerRegistered)
+            for (; userInput.ToLower() != "q";)
             {
-                playerRegistered = await RegisterPlayer();
+                Console.WriteLine("Welcome to the game of Go! Masters of Chess!\n\n\n");
+
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Register");
+                Console.WriteLine("2. Login");
+                Console.WriteLine("Q. Quit");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        bool playerRegistered = false;
+                        while (!playerRegistered)
+                        {
+                            playerRegistered = await RegisterPlayer();
+                        }
+                        break;
+                    case "2":
+                        //Login Menu
+                        break;
+                    case "Q":
+                    case "q":
+                        Console.WriteLine("Come again soon!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        Thread.Sleep(1000);
+                        break;
+                }
             }
         }
 
-        static void LoginScreen()
-        {
-            Console.WriteLine("Welcome to the game of Go! Masters of Chess!\n\n\n");
 
-
-        }
 
 
         static async Task<bool> RegisterPlayer()
@@ -34,7 +58,7 @@ namespace goGame
             ManagementClient managementClient = new ManagementClient(ServiceBusConnectionString);
             string email;
 
-            
+
             Console.WriteLine("Enter your email address to register (Ctrl+C to exit registration).");
             Console.Write("Email:");
             email = Console.ReadLine();
